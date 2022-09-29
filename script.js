f56659656f4fe40a81ee7c0b9fb189adefd595c6
Rachel Chen <rachelchen110@gmail.com>
2022-09-29 11:54:35 -0700

    center and scaled elements

--- script.js
+++ script.js
@@ -3,29 +3,46 @@ window.addEventListener('load', function() {
     const ctx = canvas.getContext('2d');
     canvas.width = 500;
     canvas.height = 500;
-    console.log(ctx);
 
     class Mandrake {
-        constructor() {
+        constructor(canvasWidth, canvasHeight) {
+            this.canvasWidth = canvasWidth;
+            this.canvasHeight = canvasHeight;
             this.image = document.getElementById('mandrake');
             this.spriteWidth = 256;
             this.spriteHeight = 256;
             this.width = this.spriteWidth;
             this.height = this.spriteHeight;
-            this.x = 0;
-            this.y = 0;
+            this.scale = 1;
+            this.x = this.canvasWidth/2 - this.width * this.scale/2;
+            this.y = this.canvasHeight/2 - this.height * this.scale/2;
             this.minFrame = 0;
-            this.maxFrame = 255;
+            this.maxFrame = 355;
+            this.frame = 0;
+            this.frameX = 3;
+            this.frameY = 7;
         }
         draw(context) {
-            context.drawImage(this.image, this.x, this.y);
+            context.drawImage(this.image, this.frameX*this.spriteWidth, this.frameY*this.spriteHeight, this.spriteWidth, 
+                this.spriteHeight, this.x, this.y, this.width * this.scale, this.height * this.scale);
 
         }
         update() {
-
+            /*if (this.frameX < 17) this.frameX++
+            else this.frameX = 0;*/
+            if (this.frame < this.maxFrame) this.frame++
+            else this.frame = this.minFrame;
         }
     }
 
-    const mandrake = new Mandrake();
-    console.log(mandrake);
+    const mandrake = new Mandrake(canvas.width, canvas.height);
+
+    function animate() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        mandrake.draw(ctx);
+        mandrake.update();
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 });
